9cf880e674ee798b3d532ca7bdb7d426fdde4ff8
Gabor Razvan <118608584+ImJinxed@users.noreply.github.com>
2025-01-27 00:22:50 +0200

    Update suiwallet.js

--- src/suiwallet.js
+++ src/suiwallet.js
@@ -1,8 +1,6 @@
 import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
- 
-// Config options for the networks you want to connect to
 const { networkConfig } = createNetworkConfig({
 	localnet: { url: getFullnodeUrl('localnet') },
 	mainnet: { url: getFullnodeUrl('mainnet') },
